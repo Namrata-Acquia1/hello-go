b2a23f612119eb70ab9d9dfcbc495fb1f18f271f
Namrata-Acquia1 <namrata.mali@acquia.com>
2024-06-25 13:31:32 +0530

    Added loop.go  to basics directory

---
+++ basics/loop.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 5
+
+	for i := 0; i < n; i++ {
+		//loop for spaces
+		for j := 0; j < n-i-1; j++ {
+			fmt.Print("  ")
+		}
+		//loop for actual stars
+		for j := 0; j <= i; j++ {
+			fmt.Print("*   ")
+		}
+		fmt.Println()
+	}
+}
